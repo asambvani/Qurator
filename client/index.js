c80f51640068274721c652b52cef501c37553a31
yangit <yan@yan.my>
2016-04-01 11:48:49 +0800

    fix/remove logo import

--- client/index.js
+++ client/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom'
 import Root from './components/Root'
 import configureStore from './store/configureStore'
 import { fetchImages } from './actions/images'
-import './img/logo.png'
 
 const store = configureStore()
 store.dispatch(fetchImages())
