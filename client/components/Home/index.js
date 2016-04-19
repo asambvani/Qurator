cd92cd19ccc63163e9438c4b0c4980a4a9f9f5ac
Natalia.M <melikyants@gmail.com>
2016-04-19 15:52:12 +0200

    header, nav

--- client/components/Home/index.js
+++ client/components/Home/index.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react'
-import { Grid, Row, Col, Carousel, CarouselItem } from 'react-bootstrap'
+import { Grid, Row, Col, Carousel, CarouselItem, Button } from 'react-bootstrap'
 import styles from './styles'
 
 class Home extends Component {
   render() {
     return (
-      <Grid>
-        <Carousel>
-          <CarouselItem>
-            <img width={900} height={500} alt="900x500" src="/img/large/01.jpg" />
+      <Grid fluid >
+        <Carousel className={styles.carousel}>
+          <CarouselItem className={styles.carouselItem}>
+            <img width="100%" height="500px"  alt="900x500" src="/img/large/01.jpg" />
             <div className="carousel-caption">
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h3>Qurator</h3>
+              <p>For when you want art, <br />but dont know where to start.</p>
+              <div className={styles.buttonsGroup}>
+                <div >
+                <p>Click here to use our <br />algorithm and find <br />the perfect art for you!</p>
+                <Button bsStyle="primary" className={styles.btnYellow}><span className={styles.arrowBtn}></span>Qurate</Button>
+                </div>
+                <div>
+                <Button bsStyle="primary" className={styles.btnBlack}>Show Now</Button>
+              </div>
+              </div>
             </div>
           </CarouselItem>
-          <CarouselItem>
-            <img width={900} height={500} alt="900x500" src="/img/large/02.jpg" />
+          <CarouselItem className={styles.carouselItem}>
+            <img width="100%" height="500px" alt="900x500" src="/img/large/02.jpg" />
             <div className="carousel-caption">
               <h3>Second slide label</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </CarouselItem>
-          <CarouselItem>
-            <img width={900} height={500} alt="900x500" src="/img/large/03.jpg" />
+          <CarouselItem className={styles.carouselItem}>
+            <img width="100%" height="500px" alt="900x500" src="/img/large/03.jpg" />
             <div className="carousel-caption">
               <h3>Third slide label</h3>
               <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
