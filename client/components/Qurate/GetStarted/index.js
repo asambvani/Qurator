0190832059e6e1d65c2b448231d2e44d00502a86
Valery Bugakov <skymk1@gmail.com>
2016-04-21 16:27:07 +0400

    Moved take our quiz to GetStarted component

--- client/components/Qurate/GetStarted/index.js
+++ client/components/Qurate/GetStarted/index.js
@@ -4,8 +4,12 @@ import styles from './styles'
 
 const GetStarted = ({ handleStartClick }) => (
   <div className={styles.getStartedD}>
+    <h3 className={styles.take}>
+      Take our <br /><span className={styles.takeQ}>quiz</span>
+    </h3>
+    <div className={styles.separatorQ}></div>
     <p className={styles.ourA}>
-      Our algorithm will <br/>
+      Our algorithm will <br />
       suggest <span className={styles.ourAyellow}>15 photos</span> that
       match your style
     </p>
