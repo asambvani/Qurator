802e93caeaaabe321f2139e525800c38b2e60d3e
Natalia.M <melikyants@gmail.com>
2016-04-27 21:32:23 +0200

    contact us

--- client/components/Contacts/index.js
+++ client/components/Contacts/index.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react'
+import styles from './styles'
 
 class Contacts extends Component {
   render() {
     return (
-      <div className="container" >
-        <br />
-        <p>We’d love to hear from you!</p>
-        <p>
-          Feel free to contact us at <a href="mailto:info@qurator-art.com">
+      <div className="container">
+        <div className={styles.contactBox}>
+        <i className="icon icon_mail_alt"></i>
+        <p className={styles.email}>EMAIL</p>
+        <a href="mailto:info@qurator-art.com">
           info@qurator-art.com
         </a>
-        </p>
+        <p>We’d love to hear from you!</p>
         <p>
-          If you are an artist interested in joining our platform,
+          Feel free to contact us at 
+        </p>
+        </div>
+        <p className={styles.contactText}>
+          If you are an artist interested in joining our platform,<br />
           please send an email with your Instagram handle and / or
-          a link to your portfolio to artists@qurator-art.com.
+          a link <br /> to your portfolio to artists@qurator-art.com.
         </p>
-        <p>
+        <p className={styles.contactSign}>
           Love,<br />
           The Qurator Team
         </p>
